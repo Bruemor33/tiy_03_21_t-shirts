244c704c586a176ac0cfdf85731639a8748afd7a
Bruemor <arymer1@gmail.com>
2016-03-30 15:56:23 -0400

    Used local storage using Jquery. Having an issue posting more than one item to the local storage.

---
+++ app/scripts/components/cart.jsx
@@ -0,0 +1,21 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Backbone = require('backbone');
+require('backbone-react-component');
+
+
+
+var CartComponent = React.createClass({
+
+  render: function(){
+    return (
+      <div>
+        <div className="row">
+          
+        </div>
+      </div>
+    )
+  }
+})
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,47 +5,82 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 require('backbone-react-component');
 
-var ProductCatalog = require('./components/collection.jsx');
+var shirtTotal = [];
+var retrievedTotal = [];
 
-var shirts = [
-  {
-    "image": "images/traitor.jpg",
-    "title": "The Traitor",
-    "description": "A shirt based off the popular meme from episode 7.",
-    "price": 2500
-  },
-  {
-    "image": "images/traitor.jpg",
-    "title": "Bounty",
-    "description": "Our favorite bounty hunter, Buba Fett.",
-    "price": 2500
-  },
-  {
-    "image": "images/traitor.jpg",
-    "title": "Darth Female",
-    "description": "Darth Vader inspired Obey shit.",
-    "price": 2500
-  },
-  {
-    "image": "images/traitor.jpg",
-    "title": "Mandalore",
-    "description": "Mandalorian Imagery",
-    "price": 2500
-  },
-  {
-    "image": "images/traitor.jpg",
-    "title": "Star Wars",
-    "description": "Origional Star Wars Cast.",
-    "price": 2500
-  },
-  {
-    "image": "images/traitor.jpg",
-    "title": "Table",
-    "description": "Periodic Table of Star Wars.",
-    "price": 2500
-  }
-]
+$('#shirt1').click(function(e){
+  e.preventDefault();
+  var title = $('.title1').html();
+  var quantity = $('.quantity1').val();
+  var size = $('.select1 option:selected').html();
+  var time = (new Date().getHours() * 60) + new Date().getMinutes();
+  var selectionInfo = {title: title, quantity: quantity, size: size, time: time};
+  shirtTotal.push(selectionInfo);
+  localStorage.setItem('order', JSON.stringify(shirtTotal));
+});
 
-module.exports = {
-  'shirts': shirts
-}
+$('#shirt2').click(function(e){
+  e.preventDefault();
+  var title = $('.title2').html();
+  var quantity = $('.quantity2').val();
+  var size = $('.select2 option:selected').html();
+  var time = (new Date().getHours() * 60) + new Date().getMinutes();
+  var selectionInfo = {title: title, quantity: quantity, size: size, time: time};
+  shirtTotal.push(selectionInfo);
+  localStorage.setItem('order', JSON.stringify(shirtTotal));
+});
+
+$('#shirt3').click(function(e){
+  e.preventDefault();
+  var title = $('.title3').html();
+  var quantity = $('.quantity3').val();
+  var size = $('.select3 option:selected').html();
+  var time = (new Date().getHours() * 60) + new Date().getMinutes();
+  var selectionInfo = {title: title, quantity: quantity, size: size, time: time};
+  shirtTotal.push(selectionInfo);
+  localStorage.setItem('order', JSON.stringify(shirtTotal));
+});
+
+$('#shirt4').click(function(e){
+  e.preventDefault();
+  var title = $('.title4').html();
+  var quantity = $('.quantity4').val();
+  var size = $('.select4 option:selected').html();
+  var time = (new Date().getHours() * 60) + new Date().getMinutes();
+  var selectionInfo = {title: title, quantity: quantity, size: size, time: time};
+  shirtTotal.push(selectionInfo);
+  localStorage.setItem('order', JSON.stringify(shirtTotal));
+});
+
+$('#shirt5').click(function(e){
+  e.preventDefault();
+  var title = $('.title5').html();
+  var quantity = $('.quantity5').val();
+  var size = $('.select5 option:selected').html();
+  var time = (new Date().getHours() * 60) + new Date().getMinutes();
+  var selectionInfo = {title: title, quantity: quantity, size: size, time: time};
+  shirtTotal.push(selectionInfo);
+  localStorage.setItem('order', JSON.stringify(shirtTotal));
+});
+
+$('#shirt6').click(function(e){
+  e.preventDefault();
+  var title = $('.title6').html();
+  var quantity = $('.quantity6').val();
+  var size = $('.select6 option:selected').html();
+  var time = (new Date().getHours() * 60) + new Date().getMinutes();
+  var selectionInfo = {title: title, quantity: quantity, size: size, time: time};
+  shirtTotal.push(selectionInfo);
+  localStorage.setItem('order', JSON.stringify(shirtTotal));
+});
+
+console.log(shirtTotal);
+console.log(localStorage.getItem('OrderInfo'));
+
+var getShirts = JSON.parse(localStorage.getItem('order'));
+var currentTime = (new Date().getHours() * 60) + new Date().getMinutes();
+var cart = getShirts.map(function(item, index){
+  var addedTime = item.time;
+  var shirtTime = Math.floor((currentTime - addedTime)) + 1;
+  $('#table-row').append('<tr><th>' + index + '</th><td>' + item.title + '</td><td>' + item.quantity + '</td><td>' + item.size + '</td></tr>')
+})
